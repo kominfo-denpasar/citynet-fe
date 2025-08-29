1560d440c3eeccf3df5b4c270c01a4e4bca6e8b6
k0m1nf0_2024 <thezeneight@gmail.com>
2025-08-30 07:36:28 +0800

    miscs: edit konfigurasi supaya dapat diakses lewat domain

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    port: 5173
+    port: 5173,
+    allowedHosts: [             
+    	'localhost',            
+    	'127.0.0.1',            
+    	'citynet.kreatifitas.site'
+    ],                          
   }
 })
