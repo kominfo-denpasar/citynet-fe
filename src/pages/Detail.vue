46f1e3bf29c9bf7a00d402e23cd897e55fb3669f
k0m1nf0_2024 <thezeneight@gmail.com>
2025-09-06 21:21:15 +0800

    feat: tambah tab baru dan juga beberapa komponen serta edit tampilan halaman detail

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -23,10 +23,10 @@
 					<!-- Content -->
 					<div class="space-y-5 md:space-y-8">
 						<div class="space-y-3">
-							<h2 class="text-2xl font-bold md:text-3xl dark:text-white">Program Schedule</h2>
 
 							<!-- accordion -->
 							<div class="space-y-2">
+								<h2 class="text-1xl font-bold md:text-2xl dark:text-white">Program Schedule</h2>
 								<!-- Item 1 -->
 								<div class="border rounded-lg">
 									<button @click="toggle(1)"
@@ -47,9 +47,7 @@
 										<span>Programme</span>
 										<font-awesome-icon :icon="openIndex === 2 ? 'minus' : 'plus'" />
 									</button>
-									<div
-										v-show="openIndex === 2"
-										class="px-4 pb-3 dark:text-gray-400">
+									<div v-show="openIndex === 2" class="px-4 pb-3 dark:text-gray-400">
 										<!-- Card Section -->
 										<div class="max-w-5xl px-4 py-10 sm:px-6 lg:px-2 lg:py-4 mx-auto">
 											<!-- Grid -->
@@ -99,109 +97,112 @@
 									</div>
 								</div>
 							</div>
+							<!-- end accordion -->
 
-							<figure>
-								<img
-								class="w-full object-cover rounded-xl"
-								src="https://images.unsplash.com/photo-1670272505340-d906d8d77d03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
-								alt="Blog Image">
-								<figcaption
-									class="mt-3 text-sm text-center text-gray-500 dark:text-neutral-500">
-									A woman sitting at a table.
-								</figcaption>
-							</figure>
+							<!-- participants -->
+							<div class="space-y-2 py-8">
+								<h2 class="text-1xl font-bold md:text-2xl dark:text-white">Participant List</h2>
 
-							<p class="text-lg text-gray-800 dark:text-neutral-200">As we've grown, we've
-								seen how Preline has helped companies such as Spotify, Microsoft, Airbnb,
-								Facebook, and Intercom bring their designers closer together to create amazing
-								things. We've also learned that when the culture of sharing is brought in
-								earlier, the better teams adapt and communicate with one another.</p>
-
-							<p class="text-lg text-gray-800 dark:text-neutral-200">That's why we are excited to share that we now have a
-								<a
-									class="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium dark:text-blue-500"
-									href="#">free version of Preline</a>,
-								which will allow individual designers, startups and other small teams a chance
-								to create a culture of openness early on.</p>
-
-							<blockquote class="text-center p-4 sm:px-7">
-								<p
-									class="text-xl font-medium text-gray-800 md:text-2xl md:leading-normal xl:text-2xl xl:leading-normal dark:text-neutral-200">
-									To say that switching to Preline has been life-changing is an understatement. My
-									business has tripled and I got my life back.
-								</p>
-								<p class="mt-5 text-gray-800 dark:text-neutral-200">
-									Nicole Grazioso
-								</p>
-							</blockquote>
-
-							<figure>
-								<img
-									class="w-full object-cover rounded-xl"
-									src="https://images.unsplash.com/photo-1670272498380-eb330b61f3cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
-									alt="Blog Image">
-									<figcaption
-										class="mt-3 text-sm text-center text-gray-500 dark:text-neutral-500">
-										A man and a woman looking at a cell phone.
-									</figcaption>
-							</figure>
-
-							<div class="space-y-3">
-								<h3 class="text-2xl font-semibold dark:text-white">Bringing the culture of sharing to everyone</h3>
-
-								<p class="text-lg text-gray-800 dark:text-neutral-200">We know the power of
-									sharing is real, and we want to create an opportunity for everyone to try
-									Preline and explore how transformative open communication can be. Now you can
-									have a team of one or two designers and unlimited spectators (think PMs,
-									management, marketing, etc.) share work and explore the design process earlier.</p>
+								<Tabs
+									:tabs="[
+									{ id: 1, label: 'Internal', icon: 'home', component: TabOneContent },
+									{ id: 2, label: 'External', icon: 'cog', component: TabTwoContent },
+									{ id: 3, label: 'Participants', icon: 'user', component: TabThreeContent }
+									]"
+								/>
 							</div>
+							<!-- end participant -->
 
-							<ul
-								class="list-disc list-outside space-y-5 ps-5 text-lg text-gray-800 dark:text-neutral-200">
-								<li class="ps-2">Preline allows us to collaborate in real time and is a really
-									great way for leadership on the team to stay up-to-date with what everybody is
-									working on,"
+							<div class="space-y-2 py-8">
+								<h2 class="text-1xl font-bold md:text-2xl dark:text-white">Section</h2>
+
+								<p class="text-lg text-gray-800 dark:text-neutral-200">That's why we are excited to share that we now have a
 									<a
 										class="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium dark:text-blue-500"
-										href="#">said</a>
-									Stewart Scott-Curran, Intercom's Director of Brand Design.</li>
-								<li class="ps-2">Preline opened a new way of sharing. It's a persistent way for
-									everyone to see and absorb each other's work," said David Scott, Creative
-									Director at
+										href="#">free version of Preline</a>,
+									which will allow individual designers, startups and other small teams a chance
+									to create a culture of openness early on.</p>
+
+								<blockquote class="text-center p-4 sm:px-7">
+									<p
+										class="text-xl font-medium text-gray-800 md:text-2xl md:leading-normal xl:text-2xl xl:leading-normal dark:text-neutral-200">
+										To say that switching to Preline has been life-changing is an understatement. My
+										business has tripled and I got my life back.
+									</p>
+									<p class="mt-5 text-gray-800 dark:text-neutral-200">
+										Nicole Grazioso
+									</p>
+								</blockquote>
+
+								<figure>
+									<img
+										class="w-full object-cover rounded-xl"
+										src="https://images.unsplash.com/photo-1670272498380-eb330b61f3cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+										alt="Blog Image">
+										<figcaption
+											class="mt-3 text-sm text-center text-gray-500 dark:text-neutral-500">
+											A man and a woman looking at a cell phone.
+										</figcaption>
+								</figure>
+
+								<div class="space-y-3">
+									<h3 class="text-2xl font-semibold dark:text-white">Bringing the culture of sharing to everyone</h3>
+
+									<p class="text-lg text-gray-800 dark:text-neutral-200">We know the power of
+										sharing is real, and we want to create an opportunity for everyone to try
+										Preline and explore how transformative open communication can be. Now you can
+										have a team of one or two designers and unlimited spectators (think PMs,
+										management, marketing, etc.) share work and explore the design process earlier.</p>
+								</div>
+
+								<ul
+									class="list-disc list-outside space-y-5 ps-5 text-lg text-gray-800 dark:text-neutral-200">
+									<li class="ps-2">Preline allows us to collaborate in real time and is a really
+										great way for leadership on the team to stay up-to-date with what everybody is
+										working on,"
+										<a
+											class="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium dark:text-blue-500"
+											href="#">said</a>
+										Stewart Scott-Curran, Intercom's Director of Brand Design.</li>
+									<li class="ps-2">Preline opened a new way of sharing. It's a persistent way for
+										everyone to see and absorb each other's work," said David Scott, Creative
+										Director at
+										<a
+											class="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium dark:text-blue-500"
+											href="#">Eventbrite</a>.</li>
+								</ul>
+
+								<p class="text-lg text-gray-800 dark:text-neutral-200">Small teams and
+									individual designers need a space where they can watch the design process
+									unfold, both for themselves and for the people they work with – no matter if
+									it's a fellow designer, product manager, developer or client. Preline allows you
+									to invite more people into the process, creating a central place for
+									conversation around design. As those teams grow, transparency and collaboration
+									becomes integrated in how they communicate and work together.</p>
+
+								<div>
 									<a
-										class="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium dark:text-blue-500"
-										href="#">Eventbrite</a>.</li>
-							</ul>
+										class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+										href="#">
+										Plan
+									</a>
+									<a
+										class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+										href="#">
+										Web development
+									</a>
+									<a
+										class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+										href="#">
+										Free
+									</a>
+									<a
+										class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+										href="#">
+										Team
+									</a>
+								</div>
 
-							<p class="text-lg text-gray-800 dark:text-neutral-200">Small teams and
-								individual designers need a space where they can watch the design process
-								unfold, both for themselves and for the people they work with – no matter if
-								it's a fellow designer, product manager, developer or client. Preline allows you
-								to invite more people into the process, creating a central place for
-								conversation around design. As those teams grow, transparency and collaboration
-								becomes integrated in how they communicate and work together.</p>
-
-							<div>
-								<a
-									class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-									href="#">
-									Plan
-								</a>
-								<a
-									class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-									href="#">
-									Web development
-								</a>
-								<a
-									class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-									href="#">
-									Free
-								</a>
-								<a
-									class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-									href="#">
-									Team
-								</a>
 							</div>
 						</div>
 							
@@ -247,4 +248,12 @@
 			loading.value = false
 		}
 	})
+
+	// -----------------
+	import Tabs from '@/components/Tabs.vue'
+	import TabOneContent from '@/components/TabOneContent.vue'
+	import TabTwoContent from '@/components/TabTwoContent.vue'
+	import TabThreeContent from '@/components/TabThreeContent.vue'
+
+
 </script>
